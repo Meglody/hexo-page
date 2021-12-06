afea733ce022526c4d6b72de68d95d857eb2d680
葛成杰 <30755703@qq.com>
2021-12-06 10:07:37 +0800

    fix: 修复自动脚本没有判断依赖的问题

--- prebuild.bash
+++ prebuild.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cd ./selfDev/cubic
+
+if [ ! -d "./node_modules/" ];then
+    echo 'No dir named node_modules/ found, prepare to install dependencies'
+    npm ci
+fi
+
 npm run build
+
+cd ../../
