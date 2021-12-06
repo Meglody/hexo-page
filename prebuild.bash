97b7b867599fa96a4fc24506116886d3eedb4c97
葛成杰 <30755703@qq.com>
2021-12-06 10:26:30 +0800

    fix: 修复自动脚本未判断复制目标文件夹路径的问题

--- prebuild.bash
+++ prebuild.bash
@@ -3,10 +3,10 @@
 cd ./selfDev/cubic
 
 if [ ! -d "./node_modules/" ];then
-    echo 'No dir named node_modules/ found, prepare to install dependencies'
+    echo 'selfDev/cubic未找到依赖文件夹，正在安装依赖...'
     npm ci
 fi
-
+echo 'selfDev/cubic正在编译...'
 npm run build
 
 cd ../../
